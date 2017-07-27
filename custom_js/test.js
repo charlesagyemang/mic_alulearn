55f3a181a730925c5834391a3d08e75bfb47483c
charles agyemang <micnkru@gmail.com>
2017-07-27 09:25:42 +0000

    feat: Quiz features

--- custom_js/test.js
+++ custom_js/test.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
-  //alert("hey")
-})
+// Questions json
+
+var questions =
+{
+	"questions":[
+    // question objects
+    {
+      "title"     : "question 1",
+      "course"    : "P1",
+      "content"   :
+      {
+        "title"            : "what is mass avertisment",
+        "image_url"        : "https://cdn.pixabay.com/photo/2017/01/06/19/15/soap-bubble-1958650_960_720.jpg",
+        "possible_answers" : ["koobi", "shito", "kooli","correct"]
+      }
+    },
+  
+
+	]
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    //your code to run since DOM is loaded and ready
+    window.alert("hey")
+});
